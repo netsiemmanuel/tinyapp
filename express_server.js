26e9231bfeedca39b39c662a9a7e7e3a88e8cc0c
Netsanet <netsizebene@gmail.com>
2022-08-09 04:43:13 +0000

    adding a second route & template and adding a header partial

--- express_server.js
+++ express_server.js
@@ -29,6 +29,10 @@ app.get("/set", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
